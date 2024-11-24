8c53c41a0db383a6178a10bbd026b4884bebd658
Khalid <hamidzada95@gmail.com>
2024-11-24 17:55:47 +0100

    created Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
-const app = () => {
-	return (
-		<div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-			<div className="flexed top-0 -z-10 h-full w-full">
-				<div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-			</div>
-
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen relative">
+      {/* Background layer */}
+      <div className="fixed top-0 left-0 -z-10 h-full w-full bg-neutral-950"></div>
+      <div
+        className="fixed top-0 left-0 -z-10 h-full w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+      ></div>
+      
+      {/* Main content */}
       <div className="container mx-auto px-8">
-        <Navbar></Navbar>
-      </div>			
-		</div>
-	);
+        <Navbar />
+        <Hero />
+      </div>
+    </div>
+  );
 };
 
-export default app;
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import {HERO_CONTENT} from "../constants"
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
